6a7441c57976b9cf3f37dc163ec83d72b1790a7e
NickMostovoi <nick211996@gmail.com>
2022-02-23 23:07:32 +0200

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,312 @@
+var navigation = [
+    {
+        name: 'Deals'
+    },
+    {
+        name: 'Customer Service'
+    },
+    {
+        name: 'Registry'
+    },
+    {
+        name: 'Gift Cards'
+    },
+    {
+        name: 'Sell'
+    }
+];
+
+var brands = [
+    {
+        name: 'Samsung Electronics'
+    },
+    {
+        name: 'HP'
+    },
+    {
+        name: 'Acer'
+    },
+    {
+        name: 'Facebook'
+    },
+    {
+        name: 'Coopers'
+    },
+    {
+        name: 'Apple'
+    },
+    {
+        name: 'ASUS'
+    }
+];
+
+var sellers = [
+    {
+        name: 'Amazon Export Sales LLC.'
+    },
+    {
+        name: 'OnlinEmart (We record SN)'
+    },
+    {
+        name: 'Amazon.com'
+    },
+    {
+        name: 'BuyTek'
+    },
+    {
+        name: 'ROKC Electronics'
+    },
+    {
+        name: 'Lake Travis Electronics'
+    },
+    {
+        name: 'Officeware Store'
+    },
+    {
+        name: 'MoneySaver'
+    },
+    {
+        name: 'ROKC Electronics'
+    },
+    {
+        name: 'Lake Travis Electronics'
+    }
+];
+
+var products = [
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor | 12GB 2666MHz DDR4 | 512GB NVMe M.2 SSD | 8X DVD | Intel Wireless Wi-Fi 6 | Bluetooth 5.2 | Windows 10 Home',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor | 12GB 2666MHz DDR4 | 512GB NVMe M.2 SSD | 8X DVD | Intel Wireless Wi-Fi 6 | Bluetooth 5.2 | Windows 10 Home',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor | 12GB 2666MHz DDR4 | 512GB NVMe M.2 SSD | 8X DVD | Intel Wireless Wi-Fi 6 | Bluetooth 5.2 | Windows 10 Home',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor | 12GB 2666MHz DDR4 | 512GB NVMe M.2 SSD | 8X DVD | Intel Wireless Wi-Fi 6 | Bluetooth 5.2 | Windows 10 Home',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop | 10th Gen Intel Core i5-10400 6-Core Processor | 12GB 2666MHz DDR4 | 512GB NVMe M.2 SSD | 8X DVD | Intel Wireless Wi-Fi 6 | Bluetooth 5.2 | Windows 10 Home',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    }
+];
+
+
+var availability = [
+    {
+        name: 'Careers'
+    },
+    {
+        name: 'Blog'
+    },
+    {
+        name: 'About Amazon'
+    },
+    {
+        name: 'Investor Relations'
+    },
+    {
+        name: 'Amazon Devices'
+    },
+    {
+        name: 'Amazon Science'
+    }
+];
+
+var productsCarousel = [
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    },
+    {
+        img: 'img/prod2.webp',
+        title: 'Samsung Galaxy Tab S6 Lite 10.4", 64GB WiFi Tablet Angora Blue - SM-P610NZBAXAR',
+        rate: '435',
+        newPrice: '323',
+        newPriceCoins: '23',
+        oldPrice: '453',
+        offerPrice: '2313',
+        counterOffers: '18',
+        left: '6'
+    },
+    {
+        img: 'img/prod1.webp',
+        title: 'Acer Aspire TC-1660-UA92 Desktop',
+        rate: '1,232',
+        newPrice: '500',
+        newPriceCoins: '45',
+        oldPrice: '634',
+        offerPrice: '534',
+        counterOffers: '27'
+    }
+];
+
+$('.navigation__list').html(doT.template($('#navigation-template').html())({navigation: navigation}));
+$('.list__brands').html(doT.template($('#brands-template').html())({brands: brands}));
+$('.list__seller').html(doT.template($('#sellers-template').html())({sellers: sellers}));
+$('.contentContainer__products').html(doT.template($('#products-template').html())({products: products}));
+$('.footer__footerAvailability').html(doT.template($('#footerAvailability-template').html())({availability: availability}));
+$('.productCarousel__wrapper').html(doT.template($('#productsCarousel-template').html())({productsCarousel: productsCarousel}));
+
+// $('.fn-list').on('click', function(e) {
+//     if (e.target.className === 'link link_color_white list__link') {
+//         $('.fn-list .isActive').removeClass('isActive');
+//         $(e.target).parent().addClass('isActive');
+//     }
+// });
